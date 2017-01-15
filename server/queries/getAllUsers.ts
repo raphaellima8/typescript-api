98b2a9b8e9e8ea9ff3928ceb93a628dfd4970a97
Raphael Lima <raphael.aolima8@gmail.com>
2017-01-15 02:36:22 -0200

    Comportamento de busca de lista de usuários OK

--- server/queries/getAllUsers.ts
+++ server/queries/getAllUsers.ts
@@ -2,9 +2,9 @@ import {UserModel} from '../model/model';
 import {User, createUser, createUsers} from '../shared/model/createUser';
 import * as Bluebird from 'bluebird';
 
-export function getAllUsers(): Bluebird<User> {
+export function getAllUsers(): Bluebird<User[]> {
   return UserModel.findAll({
     order: ['name']
   })
-  .then(createUser)
+  .then(createUsers)
 }
