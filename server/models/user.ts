afd4b2624b8b71eafd745a20e3352b02093deb18
Raphael Lima <raphael.aolima8@gmail.com>
2017-02-01 22:49:00 -0200

    Integrando Docker

---
+++ server/models/user.ts
@@ -0,0 +1,45 @@
+import * as bcrypt from 'bcrypt';
+export default function(sequelize, DataTypes) {
+  var User = sequelize.define('User', {
+    id: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      }
+    }
+  }, {
+    classMethods: {
+      validatePassword: (encryptedPassword, password) => bcrypt.compareSync(password, encryptedPassword)
+    }
+  });
+
+  User.beforeCreate((user) => {
+      return hashPass(user);
+    })
+
+    function hashPass(user) {
+      const salt = bcrypt.genSaltSync(10);
+      user.set('password', bcrypt.hashSync(user.password, salt));
+    }
+  return User;
+};
