7e86e165c4818baec4d2562ba2e033f0bec83cf8
Raphael Lima <raphael.aolima8@gmail.com>
2017-02-23 22:19:07 -0300

    Adiciona Travis CI

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,10 +1,11 @@
-export NODE_ENV= #Parâmetros
-export DB_NAME= #Parâmetros
-export DIALECT= #Parâmetros
-export DB_USER= #Parâmetros
-export DB_PASS= #Parâmetros
-export HOST= #Parâmetros
-export SERVER_PORT= #Parâmetros
-export PG_PORT= #Parâmetros
-export DB_URL= #Parâmetros
+export NODE_ENV=test
+export DB_NAME=api_test
+export DIALECT=postgres
+export DB_USER=postgres
+export DB_PASS=pgroot
+export HOST=localhost
+export SERVER_PORT=3000
+export PG_PORT=5432
+export DB_URL=postgres://postgres:pgroot@localhost:5432/api_test
+export SECRET=s3cr3t
 mocha --compilers ts:ts-node/register server/**/test.ts
