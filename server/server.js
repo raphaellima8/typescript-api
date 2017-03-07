8b5e368912eb9e60df649076d023c168f7aea5c8
Raphael Lima <raphael.aolima8@gmail.com>
2017-03-07 10:41:27 -0300

    Compila para ES5. Teste de build

--- server/server.js
+++ server/server.js
@@ -3,18 +3,23 @@ var http = require('http');
 var debug = require('debug');
 var api_1 = require('./api/api');
 var errorHandlerApi_1 = require('./api/errorHandlerApi');
+var models = require('./models');
+var config = require('./config/env/config')();
 debug('ts-api:server');
-var port = normalizePort(process.env.PORT || 3000);
+var port = normalizePort(config.server_port);
 api_1["default"].set('port', port);
 var server = http.createServer(api_1["default"]);
-server.listen(port);
-server.on('error', onError);
-server.on('listening', listen);
+models.sequelize.sync().then(function () {
+    server.listen(port);
+    server.on('error', onError);
+    server.on('listening', listen);
+});
 api_1["default"].use(errorHandlerApi_1.errorHandlerApi);
 function listen() {
     var address = server.address();
     var bind = (typeof address === 'string') ? "Address " + address : "Port " + address.port;
     debug("Server is runnig on port " + bind);
+    console.log("Server is runnig on port " + bind);
 }
 function onError(error) {
     console.log('An error ocurred: ', error);
