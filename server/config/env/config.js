dff6fd54fb82cc0987fe579ba21430f9272583b6
Raphael Lima <raphael.aolima8@gmail.com>
2017-03-07 10:59:00 -0300

    Compila para ES5. Teste de build

--- server/config/env/config.js
+++ server/config/env/config.js
@@ -1,4 +1,4 @@
 /* jshint esversion:6*/
 module.exports = function () {
-	return require(`../env/${process.env.NODE_ENV}.env.js`);
+	return require('../env/' + process.env.NODE_ENV + '.env.js');
 };
