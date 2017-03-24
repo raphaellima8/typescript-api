ae2cb309707d4a06fa4762a3bbf775e81f8326a4
Raphael Lima <raphael.aolima8@gmail.com>
2017-03-24 16:01:03 -0300

    Code coverage com istanbul nyc

--- server/auth.js
+++ server/auth.js
@@ -10,6 +10,7 @@ function AuthConfig() {
         jwtFromRequest: passport_jwt_1.ExtractJwt.fromAuthHeader()
     };
     passport.use(new passport_jwt_1.Strategy(opts, function (jwtPayload, done) {
+        console.log('payload: ', jwtPayload);
         UserService.getById(jwtPayload.id)
             .then(function (user) {
             if (user) {
--- server/config/env/test.env.js
+++ server/config/env/test.env.js
@@ -1,13 +1,13 @@
 /* jshint esversion:6*/
 module.exports = {
-  env: process.env.NODE_ENV,
-  db: process.env.DB_NAME,
-  dialect: process.env.DIALECT,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  host: process.env.HOST,
-  server_port: process.env.SERVER_PORT,
-  pg_port: process.env.PG_PORT,
-  db_url: process.env.DB_URL,
-  secret: process.env.SECRET,
+  env: "test",
+  db: "api_test",
+  dialect: "postgres",
+  username: "postgres",
+  password: "pgroot",
+  host: "localhost",
+  server_port: 3000,
+  pg_port: 5432,
+  db_url: "postgres://postgres:pgroot@localhost:5432/api",
+  secret: "xtpo",
 };
