b252001bc638e2d6e8f98c9de929dba296809f68
Raphael Lima <raphael.aolima8@gmail.com>
2017-04-25 11:50:46 -0300

    Instancia o Service no Construtor do Controller

--- server/modules/User/controller.js
+++ server/modules/User/controller.js
@@ -4,37 +4,38 @@ var errorHandler_1 = require('../../api/responses/errorHandler');
 var successHandler_1 = require('../../api/responses/successHandler');
 var dbErrorHandler_1 = require('../../config/dbErrorHandler');
 var service_1 = require('./service');
-var UserService = new service_1.User();
+// const UserService = new User();
 var UserController = (function () {
     function UserController() {
+        this.UserService = new service_1.User();
     }
     UserController.prototype.getAll = function (req, res) {
-        UserService.getAll()
+        this.UserService.getAll()
             .then(_.partial(successHandler_1.onSuccess, res))
             .catch(_.partial(errorHandler_1.onError, res, 'Find all users failed'));
     };
     UserController.prototype.createUser = function (req, res) {
-        UserService.create(req.body)
+        this.UserService.create(req.body)
             .then(_.partial(successHandler_1.onSuccess, res))
             .catch(_.partial(dbErrorHandler_1.dbErrorHandler, res))
             .catch(_.partial(errorHandler_1.onError, res, "Could not create user"));
     };
     UserController.prototype.getById = function (req, res) {
         var userId = parseInt(req.params.id);
-        UserService.getById(userId)
+        this.UserService.getById(userId)
             .then(_.partial(successHandler_1.onSuccess, res))
             .catch(_.partial(errorHandler_1.onError, res, 'Not found'));
     };
     UserController.prototype.updateUser = function (req, res) {
         var userId = parseInt(req.params.id);
         var props = req.body;
-        UserService.update(userId, props)
+        this.UserService.update(userId, props)
             .then(_.partial(successHandler_1.onSuccess, res))
             .catch(_.partial(errorHandler_1.onError, res, 'Update User failed'));
     };
     UserController.prototype.deleteUser = function (req, res) {
         var userId = req.params.id;
-        UserService.delete(userId)
+        this.UserService.delete(userId)
             .then(_.partial(successHandler_1.onSuccess, res))
             .catch(_.partial(errorHandler_1.onError, res, 'An error ocurred to delete an User'));
     };
--- server/modules/User/controller.ts
+++ server/modules/User/controller.ts
@@ -4,20 +4,23 @@ import {onError} from '../../api/responses/errorHandler';
 import {onSuccess} from '../../api/responses/successHandler';
 import {dbErrorHandler} from '../../config/dbErrorHandler';
 import {User} from './service';
-const UserService = new User();
 
 export default class UserController {
 
-  constructor() {}
+  public UserService;
+
+  constructor() {
+    this.UserService = new User();
+  }
 
   getAll(req: Request, res: Response) {
-    UserService.getAll()
+    this.UserService.getAll()
       .then(_.partial(onSuccess, res))
       .catch(_.partial(onError, res, 'Find all users failed'));
   }
 
   createUser(req: Request, res: Response) {
-    UserService.create(req.body)
+    this.UserService.create(req.body)
       .then(_.partial(onSuccess, res))
       .catch(_.partial(dbErrorHandler, res))
       .catch(_.partial(onError, res, `Could not create user`));
@@ -25,7 +28,7 @@ export default class UserController {
 
   getById(req: Request, res: Response) {
     const userId = parseInt(req.params.id);
-    UserService.getById(userId)
+    this.UserService.getById(userId)
       .then(_.partial(onSuccess, res))
       .catch(_.partial(onError, res, 'Not found'));
   }
@@ -33,14 +36,14 @@ export default class UserController {
   updateUser(req: Request, res: Response){
     const userId = parseInt(req.params.id);
     const props = req.body;
-    UserService.update(userId, props)
+    this.UserService.update(userId, props)
       .then(_.partial(onSuccess, res))
       .catch(_.partial(onError, res, 'Update User failed'));
   }
 
   deleteUser(req: Request, res: Response){
     const userId = req.params.id;
-    UserService.delete(userId)
+    this.UserService.delete(userId)
       .then(_.partial(onSuccess, res))
       .catch(_.partial(onError, res, 'An error ocurred to delete an User'));
   }
