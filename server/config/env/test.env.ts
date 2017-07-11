011808f00d78277e338c53fc535e00ee2662d828
Raphael Lima <raphael.aolima8@gmail.com>
2017-07-11 20:17:58 -0300

    Altera configuração do postgres no travis

--- server/config/env/test.env.ts
+++ server/config/env/test.env.ts
@@ -1,12 +1,12 @@
 module.exports = {
   env: "test",
-  db: "api",
+  db: "api_test",
   dialect: "postgres",
   username: "postgres",
   password: "pgroot",
   host: "localhost",
   server_port: 3000,
   pg_port: 5432,
-  db_url: "postgres://postgres:pgroot@localhost:5432/api",
+  db_url: "postgres://postgres:pgroot@localhost:5432/api_test",
   secret: "secret",
 };
